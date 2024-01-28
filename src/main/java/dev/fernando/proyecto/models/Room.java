e410655cb8330603bf05dac728cf38a9c7acf03d
Fernando Pérez <jumbonionga+git@gmail.com>
2024-01-27 22:33:27 -0600

    Definicion del modelo

---
+++ src/main/java/dev/fernando/proyecto/models/Room.java
@@ -0,0 +1,97 @@
+package dev.fernando.proyecto.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int roomNumber;
+    private String name;
+    private boolean isBooked = false;
+    private int numAdults;
+    private int numMinors;
+    private int totalGuests;
+    
+    public Room() {
+    }
+    
+    public Room(int roomNumber, String name, boolean isBooked, int numAdults, int numMinors, int totalGuests) {
+        this.roomNumber = roomNumber;
+        this.name = name;
+        this.isBooked = isBooked;
+        this.numAdults = numAdults;
+        this.numMinors = numMinors;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+    
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public boolean isBooked() {
+        return isBooked;
+    }
+    
+    public void setBooked(boolean booked) {
+        isBooked = booked;
+    }
+    
+    public int getNumAdults() {
+        return numAdults;
+    }
+    
+    public void setNumAdults(int numAdults) {
+        this.numAdults = numAdults;
+        calculateTotalGuests();
+    }
+    
+    public int getNumMinors() {
+        return numMinors;
+    }
+    
+    public void setNumMinors(int numMinors) {
+        this.numMinors = numMinors;
+        calculateTotalGuests();
+    }
+    
+    private void calculateTotalGuests() {
+        this.totalGuests = this.numAdults + this.numMinors;
+    }
+    
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", roomNumber=" + roomNumber +
+                ", name='" + name + '\'' +
+                ", isBooked=" + isBooked +
+                ", numAdults=" + numAdults +
+                ", numMinors=" + numMinors +
+                ", totalGuests=" + totalGuests +
+                '}';
+    }
+}
