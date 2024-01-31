14550abe7cab9b5fccaf8dd496bcea7252b1c8e9
Fernando Pérez <jumbonionga+git@gmail.com>
2024-01-31 10:05:57 -0600

    Reiniciando proyecto completo

--- src/main/java/dev/fernando/proyecto/ProyectoApplication.java
+++ src/main/java/dev/fernando/proyecto/ProyectoApplication.java
@@ -1,6 +1,5 @@
 package dev.fernando.proyecto;
 
-import dev.fernando.proyecto.interfaces.IRoomService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,7 +11,6 @@ public class ProyectoApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext =
 				SpringApplication.run(ProyectoApplication.class, args);
-		IRoomService roomService = applicationContext.getBean(IRoomService.class);
 		System.out.println("Server is running...");
 	}
 
